65f0faeab651a9d5f5d2844f194567819e21bd13
Grzegorz Stokłosa <gstok@poczta.fm>
2020-01-10 00:56:11 +0100

    Add tiles list for polish version

---
+++ src/TilesLists/tiles.pl.js
@@ -0,0 +1,167 @@
+export default [
+  {
+    letter: 'a',
+    count: 9,
+    score: 1,
+  },
+  {
+    letter: 'ą',
+    count: 1,
+    score: 5,
+  },
+  {
+    letter: 'b',
+    count: 2,
+    score: 3,
+  },
+  {
+    letter: 'c',
+    count: 3,
+    score: 2,
+  },
+  {
+    letter: 'ć',
+    count: 1,
+    score: 6,
+  },
+  {
+    letter: 'd',
+    count: 3,
+    score: 2,
+  },
+  {
+    letter: 'e',
+    count: 7,
+    score: 1,
+  },
+  {
+    letter: 'ę',
+    count: 1,
+    score: 5,
+  },
+  {
+    letter: 'f',
+    count: 1,
+    score: 5,
+  },
+  {
+    letter: 'g',
+    count: 2,
+    score: 3,
+  },
+  {
+    letter: 'h',
+    count: 2,
+    score: 3,
+  },
+  {
+    letter: 'i',
+    count: 8,
+    score: 1,
+  },
+  {
+    letter: 'j',
+    count: 2,
+    score: 3,
+  },
+  {
+    letter: 'k',
+    count: 3,
+    score: 2,
+  },
+  {
+    letter: 'l',
+    count: 3,
+    score: 2,
+  },
+  {
+    letter: 'ł',
+    count: 2,
+    score: 3,
+  },
+  {
+    letter: 'm',
+    count: 3,
+    score: 2,
+  },
+  {
+    letter: 'n',
+    count: 5,
+    score: 1,
+  },
+  {
+    letter: 'ń',
+    count: 1,
+    score: 7,
+  },
+  {
+    letter: 'o',
+    count: 6,
+    score: 1,
+  },
+  {
+    letter: 'ó',
+    count: 1,
+    score: 5,
+  },
+  {
+    letter: 'p',
+    count: 3,
+    score: 2,
+  },
+  {
+    letter: 'r',
+    count: 4,
+    score: 1,
+  },
+  {
+    letter: 's',
+    count: 4,
+    score: 1,
+  },
+  {
+    letter: 'ś',
+    count: 1,
+    score: 5,
+  },
+  {
+    letter: 't',
+    count: 3,
+    score: 2,
+  },
+  {
+    letter: 'u',
+    count: 2,
+    score: 3,
+  },
+  {
+    letter: 'w',
+    count: 4,
+    score: 1,
+  },
+  {
+    letter: 'y',
+    count: 4,
+    score: 2,
+  },
+  {
+    letter: 'z',
+    count: 5,
+    score: 1,
+  },
+  {
+    letter: 'ź',
+    count: 1,
+    score: 9,
+  },
+  {
+    letter: 'ż',
+    count: 1,
+    score: 5,
+  },
+  {
+    letter: '',
+    count: 2,
+    score: 0,
+  },
+];
