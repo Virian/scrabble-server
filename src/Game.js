02c041e6ba733fa68f4c4c3781d193187ed37767
Grzegorz Stokłosa <gstok@poczta.fm>
2020-01-10 01:41:23 +0100

    Add a method to initialize a game with a random order of tiles

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,19 @@
-export default class Game {
+const shuffle = require('lodash/shuffle');
+const tiles = require('./TilesLists/tiles.pl');
+const Tile = require('./Tile');
+
+module.exports = class Game {
   constructor() {
     this.players = [];
+    this.activePlayerIndex = -1;
+    this.bag = [];
   }
-}
+
+  initGame() {
+    this.bag = tiles
+      .map((tile) => Array(tile.count).fill(new Tile(tile)))
+      .reduce((acc, curr) => acc.concat(curr), []);
+    this.bag = shuffle(this.bag);
+    this.activePlayerIndex = Math.floor(Math.random() * this.players.length);
+  }
+};
